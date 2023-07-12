004afb5d4b8a8e43bae3f61b78acf8b2b41aef7a
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-07-12 19:31:19 +0300

    Status code and model binding

---
+++ ConsoleToWebApi/Controllers/CountriesController.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleToWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [BindProperties(SupportsGet = true)]
+    public class CountriesController : ControllerBase
+    {
+        // [BindProperty(SupportsGet = true)]
+         public CountryModel Country { get; set; }
+
+        //public string Name { get; set; }
+
+        //public int Population { get; set; }
+
+        //public int Area { get; set; }
+
+        [HttpGet("")]
+        public IActionResult AddCountry()
+        {
+            return Ok($"Name={this.Country.Name}, " +
+                $"Population={this.Country.Population}, " +
+                $"Area={this.Country.Area}");
+        }
+    }
+}
